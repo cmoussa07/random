3cd383e5f151a029e5004c27d44043fd85a43546
ibrahim <moussaibrahimcoulibaly2@gmail.com>
2026-02-18 13:34:43 +0000

    Suppression de console.log de script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello !");
-
 const randomNumberButton = document.getElementById("random-number");
 randomNumberButton.addEventListener("click", () => {
   document.getElementById("resultat").textContent = Math.random();
